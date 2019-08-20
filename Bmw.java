54df7d1ebe93489c55ad13c488a7e389f2e7b123
gayathri-19 <kayadri_aselva@rediffmail.com>
2019-08-20 22:56:57 +0530

    test2

---
+++ Bmw.java
@@ -0,0 +1,14 @@
+package polymorphism;
+
+public class Bmw extends car {
+
+	public void theftcontrol()
+	{
+		System.out.println("theftcontrol");
+	}
+	
+	public void start()
+	{
+		System.out.println("bmw start");
+	}
+}
---
+++ car.java
@@ -0,0 +1,20 @@
+package polymorphism;
+
+public class car {
+	
+	public void start()
+	{
+		System.out.println("start.....start");
+	}
+	
+	public void stop()
+	{
+		System.out.println("end....end");
+	}
+	
+	public void reverse()
+	{
+		System.out.println("reverse..reverse");
+	}
+	
+}
